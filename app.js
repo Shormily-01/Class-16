5b9e682d1c3fe1d65caaad239cbed60ab11525e4
Jannatul Mawa Shormily <99941363+Shormily-01@users.noreply.github.com>
2022-04-19 15:03:38 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+var movie={
+    name:'Byomkesh Bakshi',
+    writter:'Sharadindu Bandyopadhay',
+    type:'Detective',
+    refer: 'Satyanweshi'
+
+}
+
+document.write(movie.type)
+document.write('<br>')
+document.write(movie.refer)
+
+document.write('<h2>Mathemetical Operator</h2>')
+
+var num1=6
+var num2=2
+
+document.write(num1 + num2)
+document.write('<br>')
+document.write(num1 % num2)
+document.write('<br>')
+document.write(num1 * num2)
+document.write('<br>')
+document.write(++num1)
+document.write('<br>')
+document.write(num2--)
+
+document.write('<h2>Assignment Operator</h2>')
+
+var num=4
+
+document.write(num += 12)
+document.write('<br>')
+document.write(num %= 3)
+document.write('<br>')
+document.write(num **= 2)
+
+document.write('<h2>String Operator</h2>')
+
+var firstName='Jannatul Mawa'
+var llastName='Shormily'
+
+document.write(firstName +' '+ llastName)
+
+document.write('<h2>Comparison Operator</h2>')
+
+num1=20
+num2='20'
+
+document.write(num1 == num2)
+document.write('<br>')
+document.write(num1 !== num2)
+document.write('<br>')
+document.write(num1 === num2)
+document.write('<br>')
+document.write(num1 <= num2)
+
+document.write('<h2>Logical Operator</h2>')
+
+var fbId= 'Jannatul Mawa'
+var password= 'code'
+
+if (fbId == 'Jannatul Mawa' && password == 'code') {
+    alert(Enter)
+}
+
+
+
